75b0ab7f4d7134e5da0b330ceede492d54a7cf17
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-12-20 15:37:39 +0000

    Day 19a

---
+++ day19/src/bin/day19a.rs
@@ -0,0 +1,156 @@
+// Copyright (C) 2022 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use rayon::prelude::*;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::io::{stdin, BufRead};
+
+use day19::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct State {
+    pub mats: Mats,
+    pub produces: Mats,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State {
+            mats: Mats::default(),
+            produces: Mats::ORE,
+        }
+    }
+}
+
+impl State {
+    pub fn produce(&mut self) {
+        self.mats = self.mats + self.produces;
+    }
+    pub fn build(&mut self, bp: &Blueprint, robot: &Robot) {
+        self.mats = self.mats - bp.robot_cost(robot);
+        self.produces = self.produces + robot.produces();
+    }
+    pub fn can_build(&self, bp: &Blueprint, robot: &Robot) -> bool {
+        let cmp = bp.robot_cost(robot).partial_cmp(&self.mats);
+        cmp == Some(Ordering::Equal) || cmp == Some(Ordering::Less)
+    }
+}
+
+pub type Cache = HashMap<(i32, State, i32), i32>;
+
+fn eval_state(cache: &mut Cache, bp: &Blueprint, state0: &State, left: i32) -> i32 {
+    if left == 0 {
+        cache.insert((bp.id, *state0, 0), state0.mats.geode());
+        return state0.mats.geode();
+    }
+    if let Some(result) = cache.get(&(bp.id, *state0, left)) {
+        // Cache hit!
+        return *result;
+    }
+    let mut max = autofolder::Max::<i32>::new(0);
+    // Try building
+    if state0.can_build(bp, &Robot::Geode) {
+        // If I can build geode, then I do only that
+        let mut state = *state0;
+        state.produce();
+        state.build(bp, &Robot::Geode);
+        let geodes = eval_state(cache, bp, &state, left - 1);
+        max.reduce(geodes);
+    } else if state0.can_build(bp, &Robot::Obs) {
+        // If I can build obs, then I do only that
+        let mut state = *state0;
+        state.produce();
+        state.build(bp, &Robot::Obs);
+        let geodes = eval_state(cache, bp, &state, left - 1);
+        max.reduce(geodes);
+    } else {
+        for robot in [Robot::Clay, Robot::Ore].into_iter() {
+            if state0.produces.0[robot as u32 as usize] > bp.max_cost.0[robot as u32 as usize] {
+                // I have enough, can prune this branch
+                continue;
+            }
+            if state0.can_build(bp, &robot) {
+                let mut state = *state0;
+                state.produce();
+                state.build(bp, &robot);
+                let geodes = eval_state(cache, bp, &state, left - 1);
+                max.reduce(geodes);
+            }
+        }
+        // Just produce instead
+        let mut state = *state0;
+        state.produce();
+        max.reduce(eval_state(cache, bp, &state, left - 1));
+    }
+    let max = max.into_inner().unwrap();
+    cache.insert((bp.id, *state0, left), max);
+    max
+}
+
+fn eval_blueprint(bp: &Blueprint, left: i32) -> i32 {
+    let mut cache = Cache::new();
+    eval_state(&mut cache, bp, &State::default(), left)
+}
+
+fn process(bufin: impl BufRead) -> Result<i32> {
+    let blueprints = parser::parse(bufin)?;
+    Ok(blueprints
+        .into_par_iter()
+        .map(|bp| bp.id * eval_blueprint(&bp, 24))
+        .sum())
+}
+
+#[test]
+fn test_bp1() -> Result<()> {
+    let bp1 = Blueprint::new(
+        1,
+        Mats::new(4, 0, 0, 0),
+        Mats::new(2, 0, 0, 0),
+        Mats::new(3, 14, 0, 0),
+        Mats::new(2, 0, 7, 0),
+    );
+    let geodes = eval_blueprint(&bp1, 24);
+    assert_eq!(geodes, 9);
+    Ok(())
+}
+
+#[test]
+fn test_bp1_short() -> Result<()> {
+    let bp1 = Blueprint::new(
+        1,
+        Mats::new(4, 0, 0, 0),
+        Mats::new(2, 0, 0, 0),
+        Mats::new(3, 14, 0, 0),
+        Mats::new(2, 0, 7, 0),
+    );
+    let geodes = eval_blueprint(&bp1, 19);
+    assert_eq!(geodes, 1);
+    Ok(())
+}
+
+#[test]
+fn test_bp2() -> Result<()> {
+    let bp2 = Blueprint::new(
+        2,
+        Mats::new(2, 0, 0, 0),
+        Mats::new(3, 0, 0, 0),
+        Mats::new(3, 8, 0, 0),
+        Mats::new(3, 0, 12, 0),
+    );
+    let geodes = eval_blueprint(&bp2, 24);
+    assert_eq!(geodes, 12);
+    Ok(())
+}
+
+#[test]
+fn test() -> Result<()> {
+    assert_eq!(process(EXAMPLE.as_bytes())?, 33);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    println!("{}", process(stdin().lock())?);
+    Ok(())
+}
---
+++ day19/src/lib.rs
@@ -0,0 +1,205 @@
+// Copyright (C) 2022 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+pub use color_eyre::{eyre::eyre, Result};
+use std::cmp;
+use std::cmp::Ordering;
+use std::ops;
+
+pub const EXAMPLE: &str = "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+";
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Mats(pub [i32; 4]);
+
+impl Mats {
+    pub const ORE: Mats = Mats::const_mats::<1, 0, 0, 0>();
+    pub const CLAY: Mats = Mats::const_mats::<0, 1, 0, 0>();
+    pub const OBS: Mats = Mats::const_mats::<0, 0, 1, 0>();
+    pub const GEODE: Mats = Mats::const_mats::<0, 0, 0, 1>();
+    pub fn new(ore: i32, clay: i32, obs: i32, geode: i32) -> Mats {
+        Mats([ore, clay, obs, geode])
+    }
+    pub const fn const_mats<const ORE: i32, const CLAY: i32, const OBS: i32, const GEODE: i32>(
+    ) -> Mats {
+        Mats([ORE, CLAY, OBS, GEODE])
+    }
+    pub fn geode(&self) -> i32 {
+        self.0[3]
+    }
+}
+
+impl ops::Add for Mats {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Mats([
+            self.0[0] + other.0[0],
+            self.0[1] + other.0[1],
+            self.0[2] + other.0[2],
+            self.0[3] + other.0[3],
+        ])
+    }
+}
+
+impl ops::Sub for Mats {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Mats([
+            self.0[0] - other.0[0],
+            self.0[1] - other.0[1],
+            self.0[2] - other.0[2],
+            self.0[3] - other.0[3],
+        ])
+    }
+}
+
+impl cmp::PartialOrd for Mats {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0
+            .iter()
+            .zip(other.0.iter())
+            .map(|(a, b)| a.partial_cmp(b))
+            .into_iter()
+            .reduce(|prev, current| {
+                if current == Some(Ordering::Equal) || current == prev {
+                    prev
+                } else if prev == Some(Ordering::Equal) {
+                    current
+                } else {
+                    None
+                }
+            })
+            .unwrap()
+    }
+}
+
+#[test]
+fn test_ord() {
+    assert_eq!(Mats::ORE.partial_cmp(&Mats::CLAY), None);
+    assert_eq!(
+        Mats::ORE.partial_cmp(&(Mats::ORE + Mats::ORE)),
+        Some(Ordering::Less)
+    );
+    assert_eq!(
+        (Mats::ORE + Mats::ORE + Mats::CLAY).partial_cmp(&Mats::ORE),
+        Some(Ordering::Greater)
+    );
+    assert_eq!(Mats::GEODE.partial_cmp(&Mats::GEODE), Some(Ordering::Equal));
+}
+
+#[derive(Debug)]
+pub struct Blueprint {
+    pub id: i32,
+    pub ore_cost: Mats,
+    pub clay_cost: Mats,
+    pub obs_cost: Mats,
+    pub geode_cost: Mats,
+    pub max_cost: Mats,
+}
+
+impl Blueprint {
+    pub fn new(
+        id: i32,
+        ore_cost: Mats,
+        clay_cost: Mats,
+        obs_cost: Mats,
+        geode_cost: Mats,
+    ) -> Blueprint {
+        let mut bp = Blueprint {
+            id,
+            ore_cost,
+            clay_cost,
+            obs_cost,
+            geode_cost,
+            max_cost: Mats::default(),
+        };
+        bp.max_cost = bp._max_cost();
+        bp
+    }
+    fn _max_cost(&self) -> Mats {
+        let costs = [
+            self.ore_cost,
+            self.clay_cost,
+            self.obs_cost,
+            self.geode_cost,
+        ];
+        Mats::new(
+            costs.map(|c| c.0[0]).into_iter().max().unwrap(),
+            costs.map(|c| c.0[1]).into_iter().max().unwrap(),
+            costs.map(|c| c.0[2]).into_iter().max().unwrap(),
+            costs.map(|c| c.0[3]).into_iter().max().unwrap(),
+        )
+    }
+    pub fn robot_cost(&self, robot: &Robot) -> Mats {
+        match robot {
+            Robot::Ore => self.ore_cost,
+            Robot::Clay => self.clay_cost,
+            Robot::Obs => self.obs_cost,
+            Robot::Geode => self.geode_cost,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Robot {
+    Ore = 0,
+    Clay = 1,
+    Obs = 2,
+    Geode = 3,
+}
+
+impl Robot {
+    pub const ROBOTS: [Robot; 4] = [Robot::Ore, Robot::Clay, Robot::Obs, Robot::Geode];
+    pub fn produces(&self) -> Mats {
+        match self {
+            Robot::Ore => Mats::ORE,
+            Robot::Clay => Mats::CLAY,
+            Robot::Obs => Mats::OBS,
+            Robot::Geode => Mats::GEODE,
+        }
+    }
+}
+
+pub mod parser {
+    use aoc::parser::*;
+
+    use super::*;
+
+    fn line(input: &str) -> IResult<&str, Blueprint> {
+        let (input, _) = bytes::tag("Blueprint ")(input)?;
+        let (input, id) = character::i32(input)?;
+        let (input, _) = bytes::tag(": Each ore robot costs ")(input)?;
+        let (input, ore_ore) = character::i32(input)?;
+        let ore_cost = Mats::new(ore_ore, 0, 0, 0);
+        let (input, _) = bytes::tag(" ore. Each clay robot costs ")(input)?;
+        let (input, clay_ore) = character::i32(input)?;
+        let clay_cost = Mats::new(clay_ore, 0, 0, 0);
+        let (input, _) = bytes::tag(" ore. Each obsidian robot costs ")(input)?;
+        let (input, obs_ore) = character::i32(input)?;
+        let (input, _) = bytes::tag(" ore and ")(input)?;
+        let (input, obs_clay) = character::i32(input)?;
+        let obs_cost = Mats::new(obs_ore, obs_clay, 0, 0);
+        let (input, _) = bytes::tag(" clay. Each geode robot costs ")(input)?;
+        let (input, geode_ore) = character::i32(input)?;
+        let (input, _) = bytes::tag(" ore and ")(input)?;
+        let (input, geode_obs) = character::i32(input)?;
+        let geode_cost = Mats::new(geode_ore, 0, geode_obs, 0);
+        let (input, _) = bytes::tag(" obsidian.\n")(input)?;
+        Ok((
+            input,
+            Blueprint::new(id, ore_cost, clay_cost, obs_cost, geode_cost),
+        ))
+    }
+
+    pub fn parse(mut bufin: impl BufRead) -> Result<Vec<Blueprint>> {
+        aoc::parse_with!(multi::many1(line), bufin)
+    }
+}
+
+#[test]
+fn test() -> Result<()> {
+    assert_eq!(parser::parse(EXAMPLE.as_bytes())?.len(), 2);
+    Ok(())
+}
