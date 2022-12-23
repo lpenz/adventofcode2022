6e4d0a6552007d269d76d5a1fa859ed242fb83ae
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-12-23 16:58:42 +0000

    Day 23b

--- day23/src/bin/day23b.rs
+++ day23/src/bin/day23b.rs
@@ -15,31 +15,24 @@ fn process(bufin: impl BufRead) -> Result<i32> {
     for (y, line) in input.into_iter().enumerate() {
         for (x, cell) in line.into_iter().enumerate() {
             if cell == Cell::Elf {
-                let qa = Qa::try_from((x as u16 + 20, y as u16 + 20))?;
+                let qa = Qa::try_from((x as u16 + Qa::WIDTH / 2, y as u16 + Qa::HEIGHT / 2))?;
                 elves.insert(qa);
             }
         }
     }
     let mut moves = vec![Qr::N, Qr::S, Qr::W, Qr::E];
-    for _round in 0..=10 {
-        evaluate_round(&mut elves, &mut moves)?;
+    let mut round = 0;
+    let mut moved = true;
+    while moved {
+        moved = evaluate_round(&mut elves, &mut moves)?;
+        round += 1;
     }
-    let (xmin, xmax) = elves
-        .iter()
-        .map(|qa| qa.tuple().0 as i32)
-        .collect::<autofolder::MinMax<_>>()
-        .into_inner_unwrap();
-    let (ymin, ymax) = elves
-        .iter()
-        .map(|qa| qa.tuple().1 as i32)
-        .collect::<autofolder::MinMax<_>>()
-        .into_inner_unwrap();
-    Ok((xmax - xmin + 1) * (ymax - ymin + 1) - elves.len() as i32)
+    Ok(round)
 }
 
 #[test]
 fn test() -> Result<()> {
-    assert_eq!(process(EXAMPLE.as_bytes())?, 110);
+    assert_eq!(process(EXAMPLE.as_bytes())?, 20);
     Ok(())
 }
 
--- day23/src/lib.rs
+++ day23/src/lib.rs
@@ -3,6 +3,9 @@
 // file 'LICENSE', which is part of this source code package.
 
 pub use color_eyre::{eyre::eyre, eyre::Error, Result};
+use sqrid::Qr;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub const EXAMPLE: &str = "....#..
 ..###.#
@@ -62,4 +65,61 @@ pub mod parser {
 fn test() -> Result<()> {
     assert_eq!(parser::parse(EXAMPLE.as_bytes())?.len(), 7);
     Ok(())
+}
+
+// Solving:
+
+// pub type Sqrid = sqrid::sqrid_create!(12, 12, false); // example1
+// pub type Sqrid = sqrid::sqrid_create!(14, 12, false); // example
+pub type Sqrid = sqrid::sqrid_create!(u16::MAX, u16::MAX, false);
+pub type Qa = sqrid::qa_create!(Sqrid);
+
+pub fn evaluate_round(elves: &mut HashSet<Qa>, moves: &mut Vec<Qr>) -> Result<bool> {
+    // First half:
+    let mut count = HashMap::<Qa, i32>::default();
+    let mut active = HashMap::<Qa, Qa>::new();
+    let mut moved = false;
+    for elf in elves.iter() {
+        for qr in Qr::iter::<true>() {
+            if let Ok(look) = *elf + qr {
+                if elves.contains(&look) {
+                    if let Some(mv_qr) = moves.iter().copied().find(|qr| {
+                        let mut qr = *qr + Qr::NW;
+                        !(0..3).into_iter().any(|_| {
+                            let look = (*elf + qr).unwrap();
+                            qr += Qr::NE;
+                            elves.contains(&look)
+                        })
+                    }) {
+                        // elf is active, will move to dst
+                        let dst = (*elf + mv_qr)?;
+                        active.insert(*elf, dst);
+                        *count.entry(dst).or_insert(0) += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // Second half, moves:
+    for (elf, dst) in active {
+        if count.get(&dst) == Some(&1) {
+            elves.remove(&elf);
+            elves.insert(dst);
+            moved = true;
+        }
+    }
+    let qr = moves.remove(0);
+    moves.push(qr);
+    Ok(moved)
+}
+
+pub type GridD = sqrid::grid_create!(Sqrid, char);
+
+pub fn debug(elves: &HashSet<Qa>) {
+    let mut g = GridD::repeat('.');
+    for e in elves {
+        g[e] = '#';
+    }
+    eprintln!("{}", g);
 }
