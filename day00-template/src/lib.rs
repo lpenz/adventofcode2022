68197426c0eac8b3c99d86427c8099e4f42345cc
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-01-22 19:26:03 +0000

    Template: update copyright to 2023

--- day00-template/src/lib.rs
+++ day00-template/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Leandro Lisboa Penz <lpenz@lpenz.org>
+// Copyright (C) 2023 Leandro Lisboa Penz <lpenz@lpenz.org>
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
